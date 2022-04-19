a42f6c204ca8b35d4b757c96158d12c62891b36b
miqueiasbelfort <kawasousa1554@gmail.com>
2022-04-19 08:56:48 -0300

    pet preview

--- Frontend/src/components/form/PetForm.jsx
+++ Frontend/src/components/form/PetForm.jsx
@@ -9,18 +9,43 @@ const PetForm = ({petData, btnText, handleSubmit}) => {
   const colors = ["Branco", "Preto", "Cinza", "Caramelo", "Mesclado"]
 
   function onFileChange(e) {
-
+    setPreview(Array.from(e.target.files)) //trasformando file listy em array
+    setPet({...pet, images: [...e.target.files]})
   }
   function handleChange(e) {
-
+    setPet({...pet, [e.target.name]: e.target.value})
   }
   function handleColor(e) {
+    setPet({...pet, color: e.target.options[e.target.selectedIndex].text})
+  }
 
-}
-
+  function submit(e){
+    e.preventDefault()
+    //console.log(pet)
+    //handleSubmit(pet)
+  }
 
   return (
-    <form className={formStyles.form_container}>
+    <form onSubmit={submit} className={formStyles.form_container}>
+        <div className={formStyles.preview_pet_images}>
+          {preview.length > 0
+            ? preview.map((image, index) => (
+                <img 
+                  src={URL.createObjectURL(image)} 
+                  alt={pet.name} 
+                  key={`${pet.name}-${index}`} 
+                />
+            )) : 
+              pet.images && 
+              pet.images.map((image, index) => (
+                <img 
+                  src={`http://localhost:5000/images/pets/${image}`} 
+                  alt={pet.name} 
+                  key={`${pet.name}-${index}`} 
+                />
+            ))
+          }
+        </div>
         <Input 
             text="Imagens do Pet"
             type="file"
