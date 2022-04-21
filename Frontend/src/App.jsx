2b87a36b74143a9d1049097d926d148a65196bcb
miqueiasbelfort <kawasousa1554@gmail.com>
2022-04-21 10:32:20 -0300

    pet details

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Profile from "./components/pages/User/Profile"
 import MyPets from "./components/pages/Pets/MyPets"
 import AddPet from "./components/pages/Pets/AddPet"
 import EditPets from "./components/pages/Pets/EditPets"
+import PetDetail from "./components/pages/Pets/PetDetail"
 
 // Context
 import {UserProvider} from "./context/UserContext"
@@ -36,6 +37,7 @@ function App() {
             <Route path="/pet/mypets" element={<MyPets />}/>
             <Route path="/pet/add" element={<AddPet />}/>
             <Route path="/pet/edit/:id" element={<EditPets/>}/>
+            <Route path="/pet/:id" element={<PetDetail />}/>
           </Routes>
         </Container>
 
---
+++ Frontend/src/components/pages/Pets/PetDetail.jsx
@@ -0,0 +1,59 @@
+import api from "../../../utils/api"
+import React, {useEffect, useState} from 'react'
+
+import styles from "./PetDetail.module.css"
+
+import { useParams, Link } from "react-router-dom"
+
+//hooks
+import useFlashMessage from "../../../hooks/useFlashMessage"
+
+const PetDetail = () => {
+    const [pet, setPet] = useState({})
+    const {id} = useParams()
+    const {setFlashMessage} = useFlashMessage()
+    const [token] = useState(localStorage.getItem("token") || '')
+
+    useEffect(() => {
+        
+        api.get(`/pets/${id}`).then(response => {
+            setPet(response.data.pet)
+        })
+
+    }, [id])
+
+  return (
+    <>
+        {pet.name && (
+            <section className={styles.pet_details_container}>
+                <div className={styles.pet_details_header}>
+                    <h1>Conhecendo o Pet: {pet.name}</h1>
+                    <p>Se tiver interesse, maqure uma visita para conhecê-lo</p>
+                </div>
+                <div className={styles.pet_images}>
+                    {pet.images.map((image, index) => (
+                        <img 
+                            src={`http://localhost:5000/images/pets/${image}`}
+                            alt={pet.name}
+                            key={index}
+                        />
+                    ))}
+                </div>
+                <p>
+                    <span className="bold">Peso:</span> {pet.weight}kg
+                </p>
+                <p>
+                    <span className="bold">Idade:</span> {pet.age} anos
+                </p>
+                {token ? (
+                    <button>Solicitar uma visita</button>
+                ) : (
+                    <p>Você precisa <Link to="/register">criar uma conta</Link> para solicatar a visita</p>
+                )}
+            </section>
+        )}
+    </>
+  )
+}
+
+export default PetDetail
